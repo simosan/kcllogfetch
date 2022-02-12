40e24aa2e8b19a1ba67342d879ae02a1079b5080
simosan <m.simosan@gmail.com>
2022-02-12 17:23:47 +0900

    20220212 Update

--- src/main/java/com/simosan/kclapi/kcllogfetch/SimKinesisConsumeAppDtPos.java
+++ src/main/java/com/simosan/kclapi/kcllogfetch/SimKinesisConsumeAppDtPos.java
@@ -1,3 +1,4 @@
+
 package com.simosan.kclapi.kcllogfetch;
 
 import java.text.SimpleDateFormat;
---
+++ src/test/java/com/simosan/kclapi/kcllogfetch/SimKinesisConsumeAppTest.java
@@ -0,0 +1,15 @@
+package com.simosan.kclapi.kcllogfetch;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class SimKinesisConsumeAppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+}
